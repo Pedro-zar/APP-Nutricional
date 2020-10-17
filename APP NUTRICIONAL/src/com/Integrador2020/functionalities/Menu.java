ecda3c06d19a38735ec72b1a45648c2f6c7f0ecb
Pedro Henrique S <pedrozar@outlook.com.br>
2020-10-17 17:11:42 -0300

    Update Menu.java

--- APP NUTRICIONAL/src/com/Integrador2020/functionalities/Menu.java
+++ APP NUTRICIONAL/src/com/Integrador2020/functionalities/Menu.java
@@ -2,6 +2,10 @@ package com.Integrador2020.functionalities;
 
 public class Menu {
 
+	public String[] options = { "DIARY","UPDATE", "LOGOUT"};
+	public int currentOption = 0, maxOption = options.length-1;
+	public boolean down = false, up = false, enter = false;
+	
 	public void tick() {
 		
 	}
